a26ec0aeaa41347e38d4235f9ac675cc41cd9e39
Deusdedith Junior <104400883+DeusdedithJhunior@users.noreply.github.com>
2022-08-20 18:43:56 -0300

    ending interactions

--- Dashboard/dashboard_covid.py
+++ Dashboard/dashboard_covid.py
@@ -12,6 +12,8 @@ import numpy as np
 import pandas as pd
 import json
 
+CENTER_LAT, CENTER_LON = -14.272572694355336, -51.25567404158474
+
 # lendo o dataset
 # df = pd.read_csv("HIST_COVIDBR_13mai2021.csv", sep=";")
 
@@ -143,7 +145,7 @@ app.layout = dbc.Container(
             html.Div([
                 html.P("Selecione o tipo de dado que deseja visualizar:",
                        style={"margin-top": "25px"}),
-                dcc.Dropdown(id="location-dropdaw",
+                dcc.Dropdown(id="location-dropdown",
                              options=[{"label": j, "value": i}
                                       for i, j in select_columns.items()],
                              value="casosNovos",
@@ -194,6 +196,61 @@ def display_status(date, location):
             obitos_novos,
             )
 
+@app.callback(Output("line-graph", "figure"),
+        [
+            Input("location-dropdown", "value"), Input("location-button", "children")
+        ]
+)
+def plot_line_graph(plot_type, location):
+    if location == "BRASIL":
+        df_data_on_location = df_brasil.copy()
+    else:
+        df_data_on_location = df_states[(df_states["estado"] == location)]
+    fig2 = go.Figure(layout={"template":"plotly_dark"})
+    bar_plots = ["casosNovos", "obitosNovos"]
+
+    if plot_type in bar_plots:
+        fig2.add_trace(go.Bar(x=df_data_on_location["data"], y=df_data_on_location[plot_type]))
+    else:
+        fig2.add_trace(go.Scatter(x=df_data_on_location["data"], y=df_data_on_location[plot_type]))
+    
+    fig2.update_layout(
+        paper_bgcolor="#242424",
+        plot_bgcolor="#242424",
+        autosize=True,
+        margin=dict(l=10, r=10, b=10, t=10),
+        )
+    return fig2
+
+@app.callback(
+    Output("choropleth-map", "figure"), 
+    [Input("date-picker", "date")]
+)
+def update_map(date):
+    df_data_on_states = df_states[df_states["data"] == date]
+
+    fig = px.choropleth_mapbox(df_data_on_states, locations="estado", geojson=brasil_geo, 
+        center={"lat": CENTER_LAT, "lon": CENTER_LON},  # https://www.google.com/maps/ -> right click -> get lat/lon
+        zoom=4, color="casosAcumulado", color_continuous_scale="Redor", opacity=0.55,
+        hover_data={"casosAcumulado": True, "casosNovos": True, "obitosNovos": True, "estado": False}
+        )
+
+    fig.update_layout(paper_bgcolor="#242424", mapbox_style="carto-darkmatter", autosize=True,
+                    margin=go.layout.Margin(l=0, r=0, t=0, b=0), showlegend=False)
+    return fig
+
+@app.callback(
+    Output("location-button", "children"),
+    [Input("choropleth-map", "clickData"), Input("location-button", "n_clicks")]
+)
+def update_location(click_data, n_clicks):
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    if click_data is not None and changed_id != "location-button.n_clicks":
+        state = click_data["points"][0]["location"]
+        return "{}".format(state)
+    
+    else:
+        return "BRASIL"
 
 if __name__ == "__main__":
     app.run_server(debug=True)
